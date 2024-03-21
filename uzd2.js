e65daed014bd6d262a0200f91b0b3b3200adabfa
Zingirdas <zingirdas@googlemail.com>
2024-03-21 08:54:53 +0200

    update

--- uzd2.js
+++ uzd2.js
@@ -6,13 +6,21 @@ data.forEach((l) => {
     mas.push(l.split(" "));
 });
 
-for (let i = 0; i < mas.length; i++) {
-    for (let y = 0; y < mas.length; y++) {
-        if (i === y || i + y === mas.length - 1) {
-            mas[i][y] = 0;
-        }
-    }
+// for (let i = 0; i < mas.length; i++) {
+//     for (let y = 0; y < mas.length; y++) {
+//         if (i === y || i + y === mas.length - 1) {
+//             mas[i][y] = 0;
+//         }
+//     }
+// }
+// for (let i = 0; i < mas.length; i++) {
+//     console.log(mas[i].join(' '));
+// }
+
+let n = mas.length;
+
+for (i = 0; i < n; i++) {
+    mas[i][i] = 0
+    mas[n - 1 - i][i] = 0
 }
-for (let i = 0; i < mas.length; i++) {
-    console.log(mas[i].join(' '));
-}
+console.log(mas);
